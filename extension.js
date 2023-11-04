f500398cc11d4275256ce8c8f3fdc8e0ac845d48
anaghasid <106751197+anaghasid@users.noreply.github.com>
2023-11-04 13:21:01 +0530

    analyze2

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const openai = require('openai')
 const { execSync } = require('child_process');
 
 
