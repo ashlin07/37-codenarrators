538f6273204d113379df9eb22a37c3d459c54033
ashlin07 <furtadoashlin07@gmail.com>
2023-11-04 11:48:47 +0530

    getting input from js and output from py

---
+++ runpython.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+
+// Command to run the Python script and pass input as a command-line argument
+const pythonProcess = spawn('python', ['python_file.py', '10']);
+
+// Listen for the Python script's output
+pythonProcess.stdout.on('data', (data) => {
+  console.log('Python Output: ' + data.toString());
+});
+
+// Listen for errors
+pythonProcess.stderr.on('data', (data) => {
+  console.error('Python Error: ' + data.toString());
+});
+
+// Listen for the Python script to exit
+pythonProcess.on('close', (code) => {
+  console.log('Python process exited with code ' + code);
+});
