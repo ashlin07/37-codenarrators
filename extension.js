d2e1399c6e05cd950cfddef4b88e8e49a70136dc
ashlin07 <furtadoashlin07@gmail.com>
2023-11-04 23:19:54 +0530

    sends output to text file

--- extension.js
+++ extension.js
@@ -1,96 +1,7 @@
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// const vscode = require('vscode');
-// const { execSync } = require('child_process');
 
-
-// // This method is called when your extension is activated
-// // Your extension is activated the very first time the command is executed
-
-// /**
-//  * @param {vscode.ExtensionContext} context
-//  */
-// function activate(context) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "codenarrator" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with  registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	let disposable = vscode.commands.registerCommand('codenarrator.helloWorld', documentCodeWithGpt3 ());
-
-// 	context.subscriptions.push(disposable);
-// }
-
-// // This method is called when your extension is deactivated
-
-// function runEslint(code) {
-//     try {
-//         // Run ESLint on the code and capture the output
-//         const eslintOutput = execSync(`npx eslint --no-eslintrc -`, {
-//             input: code,
-//             encoding: 'utf-8',
-//         });
-//         return eslintOutput;
-//     } catch (error) {
-//         return error.stdout;
-//     }
-// }
-
-
-// async function documentCodeWithGpt3(code) {
-//     try {
-//         // Define a query for GPT-3 to generate documentation
-//         const query = `Document the following JavaScript code:\n\n${code}`;
-        
-//         // Use GPT-3 to generate documentation
-//         const response = await openai.Completion.create({
-//             engine: 'davinci',
-//             prompt: query,
-//             max_tokens: 100,
-//         });
-        
-//         return response.choices[0].text;
-//     } catch (error) {
-//         return error.message;
-//     }
-// }
-
-
-// const javascriptCode = `
-// 	function addNumbers(a, b) {
-//     		return a + b;
-//     	}
-//     	`;
-    
-//     const eslintOutput = runEslint(javascriptCode);
-    
-// // Step 2: Generate documentation using GPT-3
-// documentCodeWithGpt3(javascriptCode)
-//     .then((documentation) => {
-//         console.log('ESLint Output:');
-//         console.log(eslintOutput);
-
-//         console.log('\nGenerated Documentation:');
-//         console.log(documentation);
-//     })
-//     .catch((error) => {
-//             console.error(error);
-//         });
-    
-//     module.exports = {
-//         activate,
-//         deactivate
-//     }
-    
-//     function deactivate() {}
-    
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const { spawn } = require('child_process');
+const fs=require('fs');
 
 
 // this method is called when your extension is activated
@@ -116,12 +27,14 @@ function activate(context) {
             // vscode.window.showInformationMessage(selectedText)
             // const { spawn } = require('child_process');
            
-            const pythonProcess = spawn('python', ['D:\\Kodikon\\37_codenarrators\\python_file.py', selectedText]);
+            const pythonProcess = spawn('python', ['C:\\ashlin\\college study material\\sem5\\37-codenarrators\\python_file.py', selectedText]);
 
 
 // Listen for the Python script's output
             pythonProcess.stdout.on('data', (data) => {
-            console.log('Python Output: ' + data.toString());
+                const outputFile = fs.createWriteStream('C:\\ashlin\\college study material\\sem5\\37-codenarrators\\output.txt', { flags: 'a' });
+                const outputData=data.toString()
+                outputFile.write(outputData + '\n');
             });
 
             // Listen for errors
--- runpython.js
+++ runpython.js
@@ -1,11 +1,14 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
 
 // Command to run the Python script and pass input as a command-line argument
 const pythonProcess = spawn('python', ['python_file.py', ' def calculate_square(x):     return x ** 2']);
 
 // Listen for the Python script's output
 pythonProcess.stdout.on('data', (data) => {
-  console.log('Python Output: ' + data.toString());
+  const outputFile = fs.createWriteStream('output.txt', { flags: 'a' });
+  const outputData=data.toString()
+  outputFile.write(outputData + '\n');
 });
 
 // Listen for errors
